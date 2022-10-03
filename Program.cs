2cf7480f444842f894338d65c75b08e1d2db886e
Дарья <@LAPTOP-E9K2RNCV>
2022-10-03 21:31:29 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+{
+    int second, fourth, sixth, eighth;
+    Console.WriteLine("Введите число x:");
+    second = Convert.ToInt32(Console.ReadLine());
+    fourth = Convert.ToInt32(second / 166);
+    sixth = Convert.ToInt32(second % 166);
+    eighth = Convert.ToInt32((second * 10) + fourth);
+
+
+
+    Console.WriteLine("Ответ:{0}", eighth );
+    Console.ReadKey();
+}
+
+
